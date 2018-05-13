3c05d69a067f669f53a45b92eaa83865e2ca437c
ekrylov <ekrylov@cinimex.ru>
2018-05-13 22:20:22 +0300

    add WebSecurityConfigurer

--- src/main/java/ru/evgenekrylov/SocialApplication.java
+++ src/main/java/ru/evgenekrylov/SocialApplication.java
@@ -3,13 +3,26 @@ package ru.evgenekrylov;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SocialApplication {
+public class SocialApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SocialApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
     }
 
 }
